e3ee6fb36b954c619c16b5dadeb427e7cfbf93ea
oxygene4 <5797565@gmail.com>
2022-05-29 17:53:46 +0200

    chore: update app components export

---
+++ libs/app/lib/components/index.ts
@@ -0,0 +1,11 @@
+export { default as AppButton } from "./Button.svelte";
+export { default as AppList } from "./list/List.svelte";
+export { default as AppListItem } from "./list/ListItem.svelte";
+export { default as AppIcon } from "./Icon.svelte";
+export { default as AppLoading } from "./Loading.svelte";
+export { default as AppMenu } from "./Menu.svelte";
+export { default as AppSelect } from "./Select.svelte";
+export { default as AppSwitch } from "./Switch.svelte";
+export { default as AppTextarea } from "./Textarea.svelte";
+export { default as AppTextField } from "./TextField.svelte";
+export { default as ValidationMessage } from "./ValidationMessage.svelte";
