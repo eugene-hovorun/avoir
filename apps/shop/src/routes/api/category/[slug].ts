1ed1a5eb6a049d883cc1297faad58a1bfbda7112
Leonid <Leonardoshubin@gmail.com>
2022-06-10 11:36:05 +0300

    drop admin methods

--- apps/shop/src/routes/api/category/[slug].ts
+++ apps/shop/src/routes/api/category/[slug].ts
@@ -1,17 +1,7 @@
 import type {RequestEvent, RequestHandlerOutput} from "@sveltejs/kit";
-import {CategoryService} from "../../../../../../libs/back/lib/sevices/category.service";
+import {CategoryService} from "$lib/back";
 
 /** @type {import('./__types/[slug]').RequestHandler} */
 export const get = async (event: RequestEvent): Promise<RequestHandlerOutput<any>> => {
     return await CategoryService.getCategoryBySlug(event);
-}
-
-/** @type {import('./__types/[slug]').RequestHandler} */
-export const put = async (event: RequestEvent): Promise<RequestHandlerOutput<any>> => {
-    return await CategoryService.updateCategory(event);
-}
-
-/** @type {import('./__types/[slug]').RequestHandler} */
-export const del = async (event: RequestEvent): Promise<RequestHandlerOutput<any>> => {
-    return await CategoryService.deleteCategory(event);
 }
