7219b04edf843f26c220dde0a29a889d26f80c42
oxygene4 <5797565@gmail.com>
2022-06-10 09:03:30 +0200

    feat: auth layout

---
+++ apps/admin/src/routes/auth/index@auth.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { AppButton, AppTextField } from "$lib/app";
+</script>
+
+<div class="p-4 sm:p-8">
+  <h1 class="text-center text-lg mb-4">Sign in</h1>
+
+  <AppTextField value="" />
+  <AppTextField value="" password />
+
+  <div class="flex gap-4">
+    <AppButton label="Forgot password" type="ghost" block />
+    <AppButton label="Log in" block />
+  </div>
+</div>
